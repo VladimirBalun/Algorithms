5743a9f135e1f009cab718fb6bf9fd98f0c02218
Balun Vladimir <balun2010@mail.ru>
2018-07-15 10:04:15 +0300

    Update ProducerAndConsumer.cpp
    Was changed Consumer ans Producer

--- Multithreading/ProducerAndConsumer.cpp
+++ Multithreading/ProducerAndConsumer.cpp
@@ -1,53 +1,53 @@
 #include <iostream>
-#include <vector>
+#include <queue>
 #include <random>
 #include <ctime>
 #include <chrono>
 #include <thread>
 #include <mutex>
+#include <condition_variable>
 
 std::mutex mutex;
 std::condition_variable conditionVar;
 
-const short BUFF_SIZE = 10;
-std::vector<int> buffer;
+std::queue<int> buffer;
 
-void produce() 
+void produce()
 {
-	while (true)
-	{
-		std::this_thread::sleep_for(std::chrono::seconds(1));
+    while (true)
+    {
+        std::this_thread::sleep_for(std::chrono::seconds(1));
 
-		std::unique_lock<std::mutex> lock(mutex);
-		conditionVar.wait(lock, [&]() { return buffer.size() < BUFF_SIZE; });
-		int generatedValue = 1 + rand() % 50;
-		std::cout << "Was produced: " << generatedValue << std::endl;
-		buffer.push_back(generatedValue);
-		conditionVar.notify_one();
-	}
+        std::unique_lock<std::mutex> lock(mutex);
+        conditionVar.wait(lock, [&]() { return buffer.size() < 2; });
+        int generatedValue = 1 + rand() % 50;
+        std::cout << "Was produced: " << generatedValue << std::endl;
+        buffer.push(generatedValue);
+        conditionVar.notify_one();
+    }
 }
 
-void consume() 
+void consume()
 {
-	while (true)
-	{
-		std::this_thread::sleep_for(std::chrono::seconds(1));
+    while (true)
+    {
+        std::this_thread::sleep_for(std::chrono::seconds(1));
 
-		std::unique_lock<std::mutex> lock(mutex);
-		conditionVar.wait(lock, [&]() { return !buffer.empty(); });
-		std::cout << "Was consumed: " << buffer.at(buffer.size() - 1) << std::endl;
-		buffer.pop_back();
-		conditionVar.notify_one();
-	}
+        std::unique_lock<std::mutex> lock(mutex);
+        conditionVar.wait(lock, [&]() { return !buffer.empty(); });
+        std::cout << "Was consumed: " << buffer.front() << std::endl;
+        buffer.pop();
+        conditionVar.notify_one();
+    }
 }
 
 int main()
-{	
-	std::thread consumer(consume);
-	std::thread producer(produce);
-	
-	consumer.detach();
-	producer.join();
+{
+    std::thread consumer(consume);
+    std::thread producer(produce);
 
-	return EXIT_SUCCESS;
+    consumer.join();
+    producer.detach();
+
+    return EXIT_SUCCESS;
 }
