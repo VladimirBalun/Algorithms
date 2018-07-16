917aab6db27e3d2d3eb1139937f4043bdd0bced3
Balun Vladimir <balun2010@mail.ru>
2018-07-16 19:12:21 +0300

    Update BubbleSorting.cpp
    Was changed bubble sorting.

--- Sorting/BubbleSorting.cpp
+++ Sorting/BubbleSorting.cpp
@@ -1,38 +1,45 @@
 #include <iostream>
 #include <vector>
 
-template <typename Type>
-void bubbleSort(Type &ar)
+template <typename Collection, typename Comparator>
+void bubbleSort(Collection& ar, size_t size, Comparator comparator)
 {
-	bool isSorting = false;
-	while(!isSorting) 
-	{
-	    isSorting = true;
-	    for(size_t i = 0; i < ar.size() - 1; i++)
-	    {
-	    	if(ar[i] > ar[i + 1])
-	    	{
-	    		std::swap(ar[i], ar[i + 1]);
-	    		isSorting = false;
-	    	}
-	    }
-	}
+    bool isSorting = false;
+    for (size_t i = 0; i < size - 1; i++)
+    {
+        isSorting = true;
+        for (size_t j = 0; j < size - 1; j++)
+        {
+            if (comparator(ar[j], ar[j + 1]))
+            {
+                std::swap(ar[j], ar[j + 1]);
+                isSorting = false;
+            }
+        }
+        if (isSorting)
+        {
+            return;
+        }
+    }
 }
 
 int main()
 {
-	std::vector<int> vector = {1, 5, 89, 2, 7, 34, 8, 5, 9, 1};
-	std::cout << "Not sorted array: ";
-	for(auto& val : vector)
-	{
-		std::cout << val << " ";
-	}
-	bubbleSort(vector);
-	std::cout << std::endl << "Sorted array: ";
-	for(auto& val : vector)
-	{
-		std::cout << val << " ";
-	}
-	return 0;
-}
+    std::vector<int> vector = {7, 9, 1, 5, 8, 1, 8, 3, 7, 3};
 
+    std::cout << "Not sorted array: ";
+    for(const auto& val : vector)
+    {
+        std::cout << val << " ";
+    }
+
+    bubbleSort(vector, vector.size(), [](int a, int b) { return a > b; });
+
+    std::cout << std::endl << "Sorted array: ";
+    for(const auto& val : vector)
+    {
+        std::cout << val << " ";
+    }
+
+    return EXIT_SUCCESS;
+}
