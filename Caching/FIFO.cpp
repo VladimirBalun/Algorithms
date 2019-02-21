0860408bc72a380f2faad25d7d7000dd505044fa
Balun Vladimir <balun2010@mail.ru>
2019-02-21 16:35:17 +0300

    refactor(caching): refactor FIFO caching algorithm
    Was refactored FIFO caching algorithm

--- Caching/FIFO.cpp
+++ Caching/FIFO.cpp
@@ -1,87 +1,87 @@
-#include <iostream>
-#include <vector>
-#include <deque>
 #include <map>
+#include <deque>
+#include <vector>
+#include <cstdint>
+#include <iostream>
 
-template <typename Key, typename Value>
-class FifoCache 
+template<typename Key, typename Value>
+class FifoCache
 {
 public:
-	FifoCache(size_t maxSize) : maxSize(maxSize) {}
-	void putElement(const std::pair<Key, Value>& element);
-	Value getElement(const Key& key);
-	bool isContains(const Key& key);
+    explicit FifoCache(std::size_t maxSize) noexcept : mMaxSize(maxSize) {}
+    void putElement(const std::pair<Key, Value>& element) noexcept;
+    Value getElement(const Key& key);
+    bool isContains(const Key& key) const noexcept;
 private:
-	size_t maxSize;
-	std::deque<std::pair<Key, Value>> elements;
+    const std::size_t mMaxSize;
+    std::deque<std::pair<Key, Value>> mElements;
 };
 
 template<typename Key, typename Value>
-void FifoCache<Key, Value>::putElement(const std::pair<Key, Value>& element)
+void FifoCache<Key, Value>::putElement(const std::pair<Key, Value>& element) noexcept
 {
-	if (elements.size() == maxSize) 
-	{
-		elements.pop_front();
-	}
-	elements.push_back(element);
+    if (mElements.size() == mMaxSize)
+        mElements.pop_front();
+    mElements.push_back(element);
 }
 
 template<typename Key, typename Value>
 Value FifoCache<Key, Value>::getElement(const Key& key)
 {
-	for (auto it = elements.begin(); it != elements.end(); it++)
-	{
-		if (it->first == key)
-		{
-			return it->second;
-		}
-	}
+    for (const auto& iterator : mElements)
+        if (iterator.first == key)
+            return iterator.second;
+
+    throw std::invalid_argument("Cache does not contain current element");
 }
 
 template<typename Key, typename Value>
-bool FifoCache<Key, Value>::isContains(const Key& key)
-{ 
-	for (auto it = elements.begin(); it != elements.end(); it++)
-	{
-		if (it->first == key) 
-		{
-			return true;
-		}
-	}
-	return false;
+bool FifoCache<Key, Value>::isContains(const Key& key) const noexcept
+{
+    for (const auto& iterator : mElements)
+        if (iterator.first == key)
+            return true;
+
+    return false;
 }
 
 int main()
 {
-	
-	std::vector<int> dataKeys = { 100, 102, 107, 101, 100, 102, 105, 104 };
-	std::map<int, std::string> dataFromDiskOrDB = { { 100, "Text from database or disk" }, 
-												    { 102, "Text from database or disk" },
-												    { 107, "Text from database or disk" },
-												    { 101, "Text from database or disk" },
-												    { 100, "Text from database or disk" },
-												    { 102, "Text from database or disk" },
-												    { 105, "Text from database or disk" },
-												    { 104, "Text from database or disk" } };
-	
-	std::vector<std::string> buffer;
-	FifoCache<int, std::string> cache(5);
-	for (const auto& key : dataKeys)
-	{
-		if (cache.isContains(key)) 
-		{
-			std::string elementFromFifoCache = cache.getElement(key);
-			std::cout << "Element with key: " << key << " was got from cache." << std::endl;
-			buffer.push_back(elementFromFifoCache);
-		}
-		else
-		{
-			std::string elementFromDiskOrDB = dataFromDiskOrDB[key];
-			std::cout << "Element with key: " << key << " was got from disk or database." << std::endl;
-			buffer.push_back(elementFromDiskOrDB);
-			cache.putElement(std::pair<int, std::string>(key, elementFromDiskOrDB));
-		}
-	}
-	
-	return EXIT_SUCCESS;
+    const std::vector<std::uint16_t> dataKeys = { 100, 102, 107, 101, 100, 102, 105, 104 };
+    const std::map<std::uint16_t, std::string> dataFromDiskOrDB = {
+        { 100, "Text from database or disk" },
+        { 102, "Text from database or disk" },
+        { 107, "Text from database or disk" },
+        { 101, "Text from database or disk" },
+        { 105, "Text from database or disk" },
+        { 104, "Text from database or disk" } 
+    };
+
+    std::vector<std::string> buffer;
+    FifoCache<std::uint16_t, std::string> cache(5);
+    try 
+    {
+        for (const auto& key : dataKeys)
+        {
+            if (cache.isContains(key))
+            {
+                const std::string elementFromFifoCache = cache.getElement(key);
+                std::cout << "Element with key: " << key << " was got from cache." << std::endl;
+                buffer.push_back(elementFromFifoCache);
+            }
+            else
+            {
+                const std::string elementFromDiskOrDB = dataFromDiskOrDB.at(key);
+                std::cout << "Element with key: " << key << " was got from disk or database." << std::endl;
+                buffer.push_back(elementFromDiskOrDB);
+                cache.putElement({ key, elementFromDiskOrDB }); // replace the element in the cache
+            }
+        }
+        return EXIT_SUCCESS;
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << "Error! Cause: " << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
 }
