16b09266c7e7690be80f57c4e049efdd7c953a31
Balun Vladimir <balun2010@mail.ru>
2019-04-25 14:57:29 +0300

    refactor(sorting): refactor bubble sorting

--- Sorting/BubbleSorting.cpp
+++ Sorting/BubbleSorting.cpp
@@ -2,9 +2,9 @@
 #include <iostream>
 
 template<typename Collection, typename Comparator, typename = typename Collection::size_type>
-void bubbleSort(Collection& collection, Comparator comparator) noexcept
+void bubble_sorting(Collection& collection, Comparator comparator) noexcept
 {
-    bool wasSorted = true;
+    bool was_sorted = true;
     for (Collection::size_type i = 0; i < collection.size(); i++) 
     {
         for (Collection::size_type j = 0; j < collection.size() - 1; j++) 
@@ -12,14 +12,12 @@ void bubbleSort(Collection& collection, Comparator comparator) noexcept
             if (comparator(collection[j], collection[j + 1])) 
             {
                 std::swap(collection[j], collection[j + 1]);
-                wasSorted = false;
+                was_sorted = false;
             }
         }
 
-        if (wasSorted)
-        {
+        if (was_sorted)
             return;
-        }
     }
 }
 
@@ -28,14 +26,14 @@ int main()
     std::vector<int> vector = { 7, 9, 1, 5, 8, 1, 8, 3, 7, 3 };
 
     std::cout << "Not sorted array: ";
-    for (const auto& val : vector)
-        std::cout << val << " ";
+    for (const auto& value : vector)
+        std::cout << value << " ";
 
-    bubbleSort(vector, [] (int a, int b) -> bool { return a > b; });
+    bubble_sorting(vector, [] (int a, int b) -> bool { return a > b; });
 
-    std::cout << std::endl << "Sorted array: ";
-    for (const auto& val : vector)
-        std::cout << val << " ";
+    std::cout << "\nSorted array: ";
+    for (const auto& value : vector)
+        std::cout << value << " ";
 
     return EXIT_SUCCESS;
 }
