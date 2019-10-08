278fa9e72e7918dad78345a99b5929836356072c
VladimirBalun <balun2010@mail.ru>
2019-10-08 22:34:03 +0300

    refactor(search): improve linear search algorithm
    Was improved linear search algorithm.

--- Search/LinearSearch.cpp
+++ Search/LinearSearch.cpp
@@ -1,12 +1,16 @@
 #include <vector>
 #include <iostream>
 
-template<typename Collection, typename ValueType>
-int linearSearch(Collection& array, ValueType value)
+template<typename Collection>
+int linear_search(const Collection& collection, const typename Collection::value_type& value) noexcept
 {
-    for (typename Collection::size_type i = 0; i < array.size(); i++)
-        if(array[i] == value)
+    for (typename Collection::size_type i = 0; i < collection.size(); ++i)
+    {
+        if (collection[i] == value)
+        {
             return i;
+        }
+    }
 
     return -1;
 }
@@ -14,6 +18,6 @@ int linearSearch(Collection& array, ValueType value)
 int main()
 {
     std::vector<int> vector = { 1, 5, 89, 2, 7, 34, 8, 5, 9, 1 };
-    std::cout << "Index: " << linearSearch(vector, 5) << std::endl;
-    return 0;
+    std::cout << "Index: " << linear_search(vector, 5) << std::endl;
+    return EXIT_SUCCESS;
 }
