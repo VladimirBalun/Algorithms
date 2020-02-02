35b41799c5c8a504e35fd1dd337ae281ef63e034
VladimirBalun <balun2010@mail.ru>
2020-02-02 21:55:54 +0300

    refactor(graphs): improve DFS algorithm

--- Graphs/DFS.cpp
+++ Graphs/DFS.cpp
@@ -3,19 +3,53 @@
 #include <iostream>
 #include <forward_list>
 
+struct AdjacencyList
+{
+    using view_type = std::vector<std::vector<int>>;
+};
+
 struct AdjacencyMatrix
 {
-    using view_type = std::vector<std::vector<int> >;
+    using view_type = std::vector<std::vector<int>>;
 };
+
+template<typename T>
+using GraphViewType = typename T::view_type;
 
 namespace details
 {
 
-    template<typename GraphViewType>
-    std::forward_list<int> dfs_impl(int start_vertex, const AdjacencyMatrix::view_type& graph_view);
+    template<typename T>
+    std::forward_list<int> dfs_impl(int start_vertex, const GraphViewType<T>& graph_view);
 
     template<>
-    inline std::forward_list<int> dfs_impl<AdjacencyMatrix>(int start_vertex, const typename AdjacencyMatrix::view_type& graph_view)
+    inline std::forward_list<int> dfs_impl<AdjacencyList>(int start_vertex, const GraphViewType<AdjacencyList>& graph_view)
+    {
+        std::stack<int> stack_helper{};
+        stack_helper.push(start_vertex);
+        std::forward_list<int> result{};
+        std::vector<bool> visited_vertices(graph_view.size());
+        while (!stack_helper.empty())
+        {
+            const int current_vertex = stack_helper.top();
+            stack_helper.pop();
+            result.push_front(current_vertex);
+            const std::vector<int>& row_vertices = graph_view.at(current_vertex);
+            for (int vertex : row_vertices)
+            {
+                if (!visited_vertices.at(vertex))
+                {
+                    visited_vertices.at(vertex) = true;
+                    stack_helper.push(vertex);
+                }
+            }
+        }
+
+        return result;
+    }
+
+    template<>
+    inline std::forward_list<int> dfs_impl<AdjacencyMatrix>(int start_vertex, const GraphViewType<AdjacencyMatrix>& graph_view)
     {
         std::stack<int> stack_helper{};
         stack_helper.push(start_vertex);
@@ -30,7 +64,7 @@ namespace details
             for (std::size_t i = 0u; i < row_vertices.size(); ++i)
             {
                 const int suggested_vertex = row_vertices.at(i);
-                if ((suggested_vertex > 0) && (!visited_vertices.at(i)))
+                if ( (suggested_vertex > 0) && (!visited_vertices.at(i)) )
                 {
                     visited_vertices.at(i) = true;
                     stack_helper.push(i);
@@ -43,11 +77,10 @@ namespace details
 
 }
 
-
-template<typename GraphViewType>
-std::forward_list<int> dfs(int start_vertex, const typename GraphViewType::view_type& graph_view)
+template<typename T>
+std::forward_list<int> dfs(int start_vertex, const GraphViewType<T>& graph_view)
 {
-    return details::dfs_impl<GraphViewType>(start_vertex, graph_view);
+    return details::dfs_impl<T>(start_vertex, graph_view);
 }
 
 int main()
@@ -60,22 +93,44 @@ int main()
     //               \     
     //                6    
 
-    AdjacencyMatrix::view_type adjacency_matrix = {
-        { 0, 1, 1, 0, 0, 0, 0 },
-        { 1, 0, 0, 1, 1, 0, 0 },
-        { 1, 0, 0, 0, 0, 1, 0 },
-        { 0, 1, 0, 0, 1, 0, 0 },
-        { 0, 1, 0, 1, 0, 0, 0 },
-        { 0, 0, 0, 0, 0, 0, 1 },
-        { 0, 0, 0, 0, 0, 1, 0 }
-    };
-
-    std::cout << "DFS for adjacency matrix: ";
-    std::forward_list<int> result = dfs<AdjacencyMatrix>(0, adjacency_matrix);
-    result.reverse();
-    for (const int vertex : result)
     {
-        std::cout << vertex << "   ";
+        GraphViewType<AdjacencyMatrix> adjacency_matrix = {
+            { 0, 1, 1, 0, 0, 0, 0 },
+            { 1, 0, 0, 1, 1, 0, 0 },
+            { 1, 0, 0, 0, 0, 1, 0 },
+            { 0, 1, 0, 0, 1, 0, 0 },
+            { 0, 1, 0, 1, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 1 },
+            { 0, 0, 0, 0, 0, 1, 0 }
+        };
+
+        std::cout << "DFS for adjacency matrix: ";
+        std::forward_list<int> result = dfs<AdjacencyMatrix>(0, adjacency_matrix);
+        result.reverse();
+        for (const int vertex : result)
+        {
+            std::cout << vertex << "   ";
+        }
+    }
+
+    {
+        GraphViewType<AdjacencyList> adjacency_list = {
+            { 1, 2 },
+            { 0, 3, 4 },
+            { 0, 5 },
+            { 1, 4 },
+            { 1, 3 },
+            { 2, 6 },
+            { 5 }
+        };
+
+        std::cout << "\nDFS for adjacency list: ";
+        std::forward_list<int> result = dfs<AdjacencyList>(0, adjacency_list);
+        result.reverse();
+        for (const int vertex : result)
+        {
+            std::cout << vertex << "   ";
+        }
     }
 
     return EXIT_SUCCESS;
