7af316ee6b8a9e0ccdd0eb5a8005094aa064faba
VladimirBalun <balun2010@mail.ru>
2020-02-03 21:00:20 +0300

    refactor(graphs): improve BFS algorithm

--- Graphs/BFS.cpp
+++ Graphs/BFS.cpp
@@ -3,19 +3,53 @@
 #include <iostream>
 #include <forward_list>
 
+struct AdjacencyList
+{
+    using view_type = std::vector<std::vector<int>>;
+};
+
 struct AdjacencyMatrix
 {
-    using view_type = std::vector<std::vector<int> >;
+    using view_type = std::vector<std::vector<int>>;
 };
+
+template<typename T>
+using GraphViewType = typename T::view_type;
 
 namespace details
 {
 
-    template<typename GraphViewType>
-    std::forward_list<int> bfs_impl(int start_vertex, const AdjacencyMatrix::view_type& graph_view);
+    template<typename T>
+    std::forward_list<int> bfs_impl(int start_vertex, const GraphViewType<T>& graph_view);
 
     template<>
-    inline std::forward_list<int> bfs_impl<AdjacencyMatrix>(int start_vertex, const typename AdjacencyMatrix::view_type& graph_view)
+    inline std::forward_list<int> bfs_impl<AdjacencyList>(int start_vertex, const GraphViewType<AdjacencyList>& graph_view)
+    {
+        std::queue<int> queue_helper{};
+        queue_helper.push(start_vertex);
+        std::forward_list<int> result{};
+        std::vector<bool> visited_vertices(graph_view.size());
+        while (!queue_helper.empty())
+        {
+            const int current_vertex = queue_helper.front();
+            queue_helper.pop();
+            result.push_front(current_vertex);
+            const std::vector<int>& row_vertices = graph_view.at(current_vertex);
+            for (const int vertex : row_vertices)
+            {
+                if (!visited_vertices.at(vertex))
+                {
+                    visited_vertices.at(vertex) = true;
+                    queue_helper.push(vertex);
+                }
+            }
+        }
+
+        return result;
+    }
+
+    template<>
+    inline std::forward_list<int> bfs_impl<AdjacencyMatrix>(int start_vertex, const GraphViewType<AdjacencyMatrix>& graph_view)
     {
         std::queue<int> queue_helper{};
         queue_helper.push(start_vertex);
@@ -43,11 +77,10 @@ namespace details
 
 }
 
-
-template<typename GraphViewType>
-std::forward_list<int> bfs(int start_vertex, const typename GraphViewType::view_type& graph_view)
+template<typename T>
+std::forward_list<int> bfs(int start_vertex, const GraphViewType<T>& graph_view)
 {
-    return details::bfs_impl<GraphViewType>(start_vertex, graph_view);
+    return details::bfs_impl<T>(start_vertex, graph_view);
 }
 
 int main()
@@ -60,23 +93,44 @@ int main()
     //               \     
     //                6    
 
-    AdjacencyMatrix::view_type adjacency_matrix = {
-        { 0, 1, 1, 0, 0, 0, 0 },
-        { 1, 0, 0, 1, 1, 0, 0 },
-        { 1, 0, 0, 0, 0, 1, 0 },
-        { 0, 1, 0, 0, 1, 0, 0 },
-        { 0, 1, 0, 1, 0, 0, 0 },
-        { 0, 0, 0, 0, 0, 0, 1 },
-        { 0, 0, 0, 0, 0, 1, 0 }
-    };
-
-
-    std::cout << "BFS for adjacency matrix: ";
-    std::forward_list<int> result = bfs<AdjacencyMatrix>(0, adjacency_matrix);
-    result.reverse();
-    for (const int vertex : result)
     {
-        std::cout << vertex << "   ";
+        GraphViewType<AdjacencyMatrix> adjacency_matrix = {
+            { 0, 1, 1, 0, 0, 0, 0 },
+            { 1, 0, 0, 1, 1, 0, 0 },
+            { 1, 0, 0, 0, 0, 1, 0 },
+            { 0, 1, 0, 0, 1, 0, 0 },
+            { 0, 1, 0, 1, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 1 },
+            { 0, 0, 0, 0, 0, 1, 0 }
+        };
+
+        std::cout << "BFS for adjacency matrix: ";
+        std::forward_list<int> result = bfs<AdjacencyMatrix>(0, adjacency_matrix);
+        result.reverse();
+        for (const int vertex : result)
+        {
+            std::cout << vertex << "   ";
+        }
+    }
+
+    {
+        GraphViewType<AdjacencyList> adjacency_list = {
+            { 1, 2 },
+            { 0, 3, 4 },
+            { 0, 5 },
+            { 1, 4 },
+            { 1, 3 },
+            { 2, 6 },
+            { 5 }
+        };
+
+        std::cout << "\nBFS for adjacency list: ";
+        std::forward_list<int> result = bfs<AdjacencyList>(0, adjacency_list);
+        result.reverse();
+        for (const int vertex : result)
+        {
+            std::cout << vertex << "   ";
+        }
     }
 
     return EXIT_SUCCESS;
